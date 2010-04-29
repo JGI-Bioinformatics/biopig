66ae9114bf21b261f3e856c7d98ce4a8f05d1f21
Karan <kbhatia@belize.jgi-psf.org>
2010-04-29 10:12:29 -0700

    added cassandra 0.6.1 source as submodule and added various non-maven libs to the nonmavenlibs directory

--- nonmavenlibs/installLibsInMaven.sh
+++ nonmavenlibs/installLibsInMaven.sh
@@ -4,3 +4,11 @@ mvn install:install-file -Dfile=apache-cassandra-0.6.1.jar -DgroupId=apache-cass
 
 mvn install:install-file -Dfile=libthrift-r917130.jar -DgroupId=thrift -DartifactId=libthrift -Dversion=0.6.0 -Dpackaging=jar -DgeneratePom=true
 
+mvn install:install-file -Dfile=flexjson.jar -DgroupId=flexjson -DartifactId=flexjson -Dversion=1.7.0 -Dpackaging=jar -DgeneratePom=true
+
+mvn install:install-file -Dfile=json-simple-1.1.jar -DgroupId=jsonsimple -DartifactId=jsonsimple -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true
+
+mvn install:install-file -Dfile=high-scale-lib.jar -DgroupId=high-scale-lib -DartifactId=high-scale-lib -Dversion=UNKNOWN -Dpackaging=jar -DgeneratePom=true
+
+mvn install:install-file -Dfile=clhm-production.jar -DgroupId=com.reardencommerce -DartifactId=clhm -Dversion=UNKNOWN -Dpackaging=jar -DgeneratePom=true
+
