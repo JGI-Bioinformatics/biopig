9bbe7e6a129c24fa34bd578acaa10497eac298cd
karan.bhatia <karan.bhatia@gmail.com>
2010-05-03 09:52:11 -0700

    added block readers for fasta files
    block readers allow map tasks to read a set of sequences, not one sequence
    at a time.  so one map task will run per slice of data.
    
    `

---
+++ pipelineLib/src/main/java/gov/jgi/meta/hadoop/input/FastaBlockLineReader.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright (c) 2010, Joint Genome Institute (JGI) United States Department of Energy
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the JGI.
+ * 4. Neither the name of the JGI nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY JGI ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL JGI BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package gov.jgi.meta.hadoop.input;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A class that provides a line reader from an input stream.
+ */
+public class FastaBlockLineReader {
+  private static final int DEFAULT_BUFFER_SIZE = 64 * 1024;
+  private int bufferSize = DEFAULT_BUFFER_SIZE;
+  private InputStream in;
+  private byte[] buffer;
+  // the number of bytes of real data in the buffer
+  private int bufferLength = 0;
+  // the current position in the buffer
+  private int bufferPosn = 0;
+
+  private static final byte CR = '\r';
+  private static final byte LF = '\n';
+  private static final byte seperator = '>';
+
+  /**
+   * Create a line reader that reads from the given stream using the
+   * default buffer-size (64k).
+   * @param in The input stream
+   * @throws java.io.IOException
+   */
+  public FastaBlockLineReader(InputStream in) {
+    this(in, DEFAULT_BUFFER_SIZE);
+  }
+
+  /**
+   * Create a line reader that reads from the given stream using the
+   * given buffer-size.
+   * @param in The input stream
+   * @param bufferSize Size of the read buffer
+   * @throws java.io.IOException
+   */
+  public FastaBlockLineReader(InputStream in, int bufferSize) {
+    this.in = in;
+    this.bufferSize = bufferSize;
+    this.buffer = new byte[this.bufferSize];
+  }
+
+  /**
+   * Create a line reader that reads from the given stream using the
+   * <code>io.file.buffer.size</code> specified in the given
+   * <code>Configuration</code>.
+   * @param in input stream
+   * @param conf configuration
+   * @throws java.io.IOException
+   */
+  public FastaBlockLineReader(InputStream in, Configuration conf) throws IOException {
+    this(in, conf.getInt("io.file.buffer.size", DEFAULT_BUFFER_SIZE));
+  }
+
+  /**
+   * Close the underlying stream.
+   * @throws java.io.IOException
+   */
+  public void close() throws IOException {
+    in.close();
+  }
+
+  /**
+   * Read one line from the InputStream into the given Text.  A line
+   * can be terminated by one of the following: '\n' (LF) , '\r' (CR),
+   * or '\r\n' (CR+LF).  EOF also terminates an otherwise unterminated
+   * line.
+   *
+   * @param str the object to store the given line (without newline)
+   * @param maxLineLength the maximum number of bytes to store into str;
+   *  the rest of the line is silently discarded.
+   * @param maxBytesToConsume the maximum number of bytes to consume
+   *  in this call.  This is only a hint, because if the line cross
+   *  this threshold, we allow it to happen.  It can overshoot
+   *  potentially by as much as one buffer length.
+   *
+   * @return the number of bytes read including the (longest) newline
+   * found.
+   *
+   * @throws java.io.IOException if the underlying stream throws
+   */
+  public int readLine(Text key, Text str, int maxLineLength,
+                      int maxBytesToConsume) throws IOException {
+    /* We're reading data from in, but the head of the stream may be
+     * already buffered in buffer, so we have several cases:
+     * 1. No newline characters are in the buffer, so we need to copy
+     *    everything and read another buffer from the stream.
+     * 2. An unambiguously terminated line is in buffer, so we just
+     *    copy to str.
+     * 3. Ambiguously terminated line is in buffer, i.e. buffer ends
+     *    in CR.  In this case we copy everything up to CR to str, but
+     *    we also need to see what follows CR: if it's LF, then we
+     *    need consume LF as well, so next call to readLine will read
+     *    from after that.
+     * We use a flag prevCharCR to signal if previous character was CR
+     * and, if it happens to be at the end of the buffer, delay
+     * consuming it until we have a chance to look at the char that
+     * follows.
+     */
+    str.clear();
+    key.clear();
+    int txtLength = 0; //tracks str.getLength(), as an optimization
+    int recordEnd = 0;
+    int seenData = 0;
+    int newline = 0;
+    int keySeen = 0;
+    //int newlineLength = 0; //length of terminating newline
+    //boolean prevCharCR = false; //true of prev char was CR
+    long bytesConsumed = 0;
+    do {
+      int startPosn = bufferPosn; //starting from where we left off the last time
+      if (bufferPosn >= bufferLength) {
+        startPosn = bufferPosn = 0;
+//        if (prevCharCR)
+//          ++bytesConsumed; //account for CR from previous read
+        bufferLength = in.read(buffer);
+        if (bufferLength <= 0)
+          break; // EOF
+      }
+      for (; bufferPosn < bufferLength; ++bufferPosn) { //search for record end
+
+        if (buffer[bufferPosn] == CR || buffer[bufferPosn] == LF) {
+            // CR and LF should be ignored
+            newline = 1;
+            ++bufferPosn;
+            break;
+        } else if (buffer[bufferPosn] == seperator) {
+           // found seperator, now determine if we have seen any data.
+            if (seenData == 1) {
+                // means we read a record
+                recordEnd = 1;
+                break;
+            } else {
+                //++bufferPosn;
+                startPosn = bufferPosn+1; // skip till next byte
+            }
+        } else {
+            seenData = 1;  // means we saw some data
+        }
+      }
+      int readLength = bufferPosn - startPosn;
+//      if (prevCharCR && newlineLength == 0)
+//        --readLength; //CR at the end of the buffer
+      bytesConsumed += readLength;
+      int appendLength = readLength - newline;
+      if (appendLength > maxLineLength - txtLength) {
+        appendLength = maxLineLength - txtLength;
+      }
+      if (appendLength > 0) {
+        if (keySeen == 0) {
+            // set the key first
+            key.append(buffer, startPosn, appendLength);
+            if (newline == 1)
+                keySeen = 1;
+        } else {
+            str.append(buffer, startPosn, appendLength);
+            txtLength += appendLength;
+        }
+      }
+    } while (bytesConsumed < maxBytesToConsume && recordEnd == 0);
+
+    if (bytesConsumed > (long)Integer.MAX_VALUE)
+      throw new IOException("Too many bytes before newline: " + bytesConsumed);
+    return (int)bytesConsumed;
+  }
+
+  /**
+   * Read from the InputStream into the given Text.
+   * @param str the object to store the given line
+   * @param maxLineLength the maximum number of bytes to store into str.
+   * @return the number of bytes read including the newline
+   * @throws java.io.IOException if the underlying stream throws
+   */
+  public int readLine(Text key, Text str, int maxLineLength) throws IOException {
+    return readLine(key, str, maxLineLength, Integer.MAX_VALUE);
+}
+
+  /**
+   * Read from the InputStream into the given Text.
+   * @param str the object to store the given line
+   * @return the number of bytes read including the newline
+   * @throws java.io.IOException if the underlying stream throws
+   */
+  public int readLine(Text key, Text str) throws IOException {
+    return readLine(key, str, Integer.MAX_VALUE, Integer.MAX_VALUE);
+  }
+
+}
