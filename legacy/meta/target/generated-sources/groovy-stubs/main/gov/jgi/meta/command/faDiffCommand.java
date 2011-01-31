2e43259901f744d666085c6c124d4a87ede2a74f
karan.bhatia <karan.bhatia@gmail.com>
2011-01-31 11:50:20 -0500

    restructured source code:   - moved all standalone hadoop apps into legacy/   - renamed pipelineLib to biopig   - moved all old instructions, readmes to legacy/

---
+++ legacy/meta/target/generated-sources/groovy-stubs/main/gov/jgi/meta/command/faDiffCommand.java
@@ -0,0 +1,85 @@
+//
+// Generated stub from file:/Users/dev/dev/git.metagenomics/meta/src/main/groovy/gov/jgi/meta/command/faDiffCommand.groovy
+//
+
+package gov.jgi.meta.command;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import org.biojavax.bio.seq.RichSequenceIterator;
+import org.apache.cassandra.thrift.SlicePredicate;
+import org.apache.cassandra.thrift.ColumnParent;
+import org.apache.cassandra.thrift.SliceRange;
+import org.apache.cassandra.thrift.KeySlice;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.biojavax.bio.seq.RichSequence;
+import org.biojava.bio.BioException;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.cassandra.thrift.Cassandra;
+import gov.jgi.meta.MetaUtils;
+
+public class faDiffCommand
+    extends java.lang.Object
+    implements groovy.lang.GroovyObject, command
+{
+    private List flags = null;
+    public List getFlags() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setFlags(List value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private List params = null;
+    public List getParams() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setParams(List value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.String name() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public List options() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.String usage() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public int execute(List args, Map options) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public groovy.lang.MetaClass getMetaClass() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setMetaClass(groovy.lang.MetaClass metaClass) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object invokeMethod(java.lang.String name, java.lang.Object args) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object getProperty(java.lang.String name) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setProperty(java.lang.String name, java.lang.Object value) {
+        throw new InternalError("Stubbed method");
+    }
+}
---
+++ legacy/meta/target/generated-sources/groovy-stubs/main/gov/jgi/meta/command/scanCommand.java
@@ -0,0 +1,131 @@
+//
+// Generated stub from file:/Users/dev/dev/git.metagenomics/meta/src/main/groovy/gov/jgi/meta/command/scanCommand.groovy
+//
+
+package gov.jgi.meta.command;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import org.biojavax.bio.seq.RichSequenceIterator;
+import org.apache.cassandra.thrift.SlicePredicate;
+import org.apache.cassandra.thrift.ColumnParent;
+import org.apache.cassandra.thrift.SliceRange;
+import org.apache.cassandra.thrift.KeySlice;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.biojavax.bio.seq.RichSequence;
+import org.biojava.bio.BioException;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.cassandra.thrift.Cassandra;
+
+public class scanCommand
+    extends java.lang.Object
+    implements groovy.lang.GroovyObject, command
+{
+    private java.lang.String DEFAULTKEYSPACE = null;
+    public java.lang.String getDEFAULTKEYSPACE() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setDEFAULTKEYSPACE(java.lang.String value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private java.lang.String DEFAULTTABLE = null;
+    public java.lang.String getDEFAULTTABLE() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setDEFAULTTABLE(java.lang.String value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private java.lang.String DEFAULTHOST = null;
+    public java.lang.String getDEFAULTHOST() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setDEFAULTHOST(java.lang.String value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private java.lang.String DEFAULTPORT = null;
+    public java.lang.String getDEFAULTPORT() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setDEFAULTPORT(java.lang.String value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private List flags = null;
+    public List getFlags() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setFlags(List value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private List params = null;
+    public List getParams() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setParams(List value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.String name() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public List options() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.String usage() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public int execute(List args, Map options) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public static int byteArrayToInt(byte[] b) {
+        throw new InternalError("Stubbed method");
+    }
+
+    /**
+     * Convert the byte array to an int starting from the given offset.
+     *
+     * @param b The byte array
+     * @param offset The array offset
+     * @return The integer
+     */
+    public static int byteArrayToInt(byte[] b, int offset) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public groovy.lang.MetaClass getMetaClass() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setMetaClass(groovy.lang.MetaClass metaClass) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object invokeMethod(java.lang.String name, java.lang.Object args) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object getProperty(java.lang.String name) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setProperty(java.lang.String name, java.lang.Object value) {
+        throw new InternalError("Stubbed method");
+    }
+}
