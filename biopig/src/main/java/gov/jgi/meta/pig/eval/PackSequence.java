b8d697671c59b074bc54c8ae42f0c2cd2ef72a5d
hnordberg <hnordberg@lbl.gov>
2011-07-11 17:10:29 -0700

    pig UDF to pack a sequence into a, printable, compact form.

---
+++ biopig/src/main/java/gov/jgi/meta/pig/eval/PackSequence.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2010, The Regents of the University of California, through Lawrence Berkeley
+ * National Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ * following disclaimer.
+ *
+ * (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * (3) Neither the name of the University of California, Lawrence Berkeley National Laboratory, U.S. Dept.
+ * of Energy, nor the names of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You are under no obligation whatsoever to provide any bug fixes, patches, or upgrades to the
+ * features, functionality or performance of the source code ("Enhancements") to anyone; however,
+ * if you choose to make your Enhancements available either publicly, or directly to Lawrence Berkeley
+ * National Laboratory, without imposing a separate written license agreement for such Enhancements,
+ * then you hereby grant the following license: a  non-exclusive, royalty-free perpetual license to install,
+ * use, modify, prepare derivative works, incorporate into other computer software, distribute, and
+ * sublicense such enhancements or derivative works thereof, in binary and source code form.
+ */
+
+package gov.jgi.meta.pig.eval;
+
+import gov.jgi.meta.sequence.SequenceString;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+/**
+ * pack a sequence into a, printable, compact form.
+ */
+public class PackSequence extends EvalFunc<DataByteArray> {
+
+    /**
+     * Method invoked on every tuple during foreach evaluation
+     * @param input tuple; assumed to be a sequence tuple of the form <sequence>
+     * @exception java.io.IOException
+     */
+    public DataByteArray exec(Tuple input) throws IOException {
+        //String x = (String) input.get(0);
+        //byte[] y = x.getBytes();
+
+        if (input.size() == 0) {
+            return null;
+        }
+
+        if (input.get(0) == null) {
+            return null;
+        }
+        
+        String seq = input.get(0).toString();
+        byte[] y  = SequenceString.sequenceToByteArray(seq);
+        if( y == null )
+        	return null;
+        return new DataByteArray(y, 0, y.length);
+    }
+    
+    @Override
+    public Schema outputSchema(Schema input) {
+        return new Schema(new Schema.FieldSchema(null, DataType.BYTEARRAY));
+    }
+}
