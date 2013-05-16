b562acb499c1ee9ed9d7731958e4859d6ebebd51
Henrik Nordberg <henrik_nordberg@yahoo.com>
2013-05-16 11:20:53 -0700

    Python wrapper to loop over contigExtension.pig

--- apps/exp/rumen-extension/contigExtension.py
+++ apps/exp/rumen-extension/contigExtension.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
 from org.apache.pig.scripting import *
 
-allFile = 'all.txt'
-indexFile = '/users/kwwang/cloud/HiSeq_10000M.fas-generateIndexPacked.pig.out'
-contigFile = '/users/kwwang/cloud/10000M.new'
+indexFile = '/users/nordberg/cloud/HiSeq_100000M.fas-generateIndexPacked.pig.out'
+contigFile = '/users/nordberg/cloud/HiSeq_100000M.fas-generateIndexPacked.pig.out.txt'
 p = 100
 Pig.fs("rmr output")
 
 PP = Pig.compile("""
-register /global/homes/k/kwwang/local/biopig/lib/biopig-core-0.3.0-job.jar;
+register /global/homes/n/nordberg/local/biopig/lib/biopig-core-1.0.0-job.jar;
 -- load the target sequences
         readindex = load '$indexFile' using PigStorage as (seq: bytearray, kmer: bytearray);
---        store readindex into 'readindex-$i';
 -- now index the contigs
         contigs = load '$contigFile' using PigStorage as (geneid: chararray, seq: chararray);
 
         P = group contigs by $1 PARALLEL $p;
-
 
         contigs = foreach P {
             values= $1.$0;
@@ -25,14 +22,8 @@ register /global/homes/k/kwwang/local/biopig/lib/biopig-core-0.3.0-job.jar;
         }
 
         contigindex = foreach contigs generate geneid, FLATTEN(gov.jgi.meta.pig.eval.KmerGenerator(seq, 20)) as (kmer:bytearray);
---        store contigindex into 'contigindex-$i';
-        -- join reads with the contigs database
         j = join readindex by kmer, contigindex by kmer PARALLEL $p;
---        store j  into 'data.j';
-
         k = foreach j generate contigindex::geneid as contigid, gov.jgi.meta.pig.eval.UnpackSequence(readindex::seq) as readseq;
---        store k  into 'data.k';
-
         kk = distinct k PARALLEL $p;
         l = group kk by contigid PARALLEL $p;
         m = foreach l {
@@ -41,9 +32,6 @@ register /global/homes/k/kwwang/local/biopig/lib/biopig-core-0.3.0-job.jar;
         }
         -- join the contigid back with the contigs
         n = join contigs by geneid, m by $0 PARALLEL $p;
---        store n  into 'data.n';
-
-        -- n: {contigs::geneid: chararray,contigs::seq: chararray,m::group: chararray,m::a: {readseq: bytearray}}
 
         -- now assemble
         contigs = foreach n generate $0 as geneid, gov.jgi.meta.pig.aggregate.ExtendContigWithCap3($1, $3) as res:(seq:chararray, val:int);
@@ -51,9 +39,6 @@ register /global/homes/k/kwwang/local/biopig/lib/biopig-core-0.3.0-job.jar;
         contigs = foreach contigs generate $0 as geneid, res.seq as seq;
         store contigs into 'output/step-$i';
         store notextended into 'output/data-$i';
---        contigsall = load '$allFile' using PigStorage as (geneid: chararray, seq: chararray);
---        UNION contigsall, notextended;
-
 """)
 
 for i in range(50):
